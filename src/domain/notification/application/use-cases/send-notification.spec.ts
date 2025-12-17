28fb306dbb5c8101f2cbcc79f9b1a09e306d9e3a
Marlon Buosi <marlonbuosi_@hotmail.com>
2025-12-17 20:43:00 -0300

    feat: add create notification use case, repositories and tests

---
+++ src/domain/notification/application/use-cases/send-notification.spec.ts
@@ -0,0 +1,26 @@
+import { describe, expect, it } from 'vitest'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { SendNotificationUseCase } from './send-notification'
+import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-send-notification-repository'
+
+let inMemoryNotificationsRepository: InMemoryNotificationsRepository
+let sut: SendNotificationUseCase
+
+describe('Send Notification Use Case', () => {
+  beforeEach(() => {
+    inMemoryNotificationsRepository = new InMemoryNotificationsRepository(
+    )
+    sut = new SendNotificationUseCase(inMemoryNotificationsRepository)
+  })
+
+  it('should be able to send a notification', async () => {
+    const result = await sut.execute({
+      recipientId: '1',
+      title: 'Example Notification Title',
+      content: 'This is an example notification content.',
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryNotificationsRepository.items[0]).toEqual(result.value?.notification)
+  })
+})
