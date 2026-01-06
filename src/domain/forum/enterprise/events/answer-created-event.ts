eb6c7914a288af4a7f061e6cfa70942234202145
Marlon Buosi <marlonbuosi_@hotmail.com>
2026-01-06 20:19:51 -0300

    chore: reorder imports

--- src/domain/forum/enterprise/events/answer-created-event.ts
+++ src/domain/forum/enterprise/events/answer-created-event.ts
@@ -1,6 +1,6 @@
-import { UniqueEntityId } from '@/core/entities/unique-entity-id'
-import { DomainEvent } from '@/core/events/domain-event'
-import { Answer } from '../entities/answer'
+import type { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import type { DomainEvent } from '@/core/events/domain-event'
+import type { Answer } from '../entities/answer'
 
 export class AnswerCreatedEvent implements DomainEvent {
   public occurredAt: Date
